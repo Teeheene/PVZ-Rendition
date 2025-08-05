86264285a16a07405b18f8a8637452c829df21d6
Teeheene <diane.panganiban1010@gmail.com>
2025-08-05 16:38:19 +0800

    chore(init): kickstart initialization

---
+++ model/Peashooter.java
@@ -0,0 +1,12 @@
+public class Peashooter extends Plant
+{
+	public Peashooter(Position pos)
+	{
+		super("Peashooter", 100,20,100,pos);
+	}
+	
+	public int attack()
+	{
+		return atk;
+	}
+}
---
+++ model/Position.java
@@ -0,0 +1,17 @@
+public class Position
+{
+	private int x;
+	private int y;
+	
+	public Position(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+	
+	//setters and getters
+	public void setX(int x) { this.x = x; }
+	public void setY(int y) { this.y = y; }
+	public void getX() { return x; }
+	public void getY() { return y; }
+}
