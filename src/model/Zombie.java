f5a20cf881322fde1c64aaca53a8f419bbd14d8c
Teeheene <diane.panganiban1010@gmail.com>
2025-08-05 16:44:11 +0800

    chore: initialize project with base files

---
+++ src/model/Zombie.java
@@ -0,0 +1,19 @@
+public class Zombie
+{
+	private String name;
+	
+	private int hp;
+	private int atk;
+	private int spd;
+	
+	private Position pos;
+	
+	public Zombie(String name, int hp, int atk, int spd, Position pos)
+	{
+		this.name = name;
+		this.hp = hp;
+		this.atk = atk;
+		this.spd = spd;
+		this.pos = pos;
+	}
+}
